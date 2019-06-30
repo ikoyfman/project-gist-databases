13f1aa6e480e381927973667a28fd4c9fef4902e
Ilya Koy <Ilya.koy@outlook.com>
2019-06-30 07:02:20 -0400

    tests_working, SQL Formatting has issues

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,31 @@
 import requests
+import sqlite3
+
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    github_api_url = "https://api.github.com/users/{}/gists".format(username)
+    
+    response = requests.get(github_api_url)
+    response.raise_for_status()
+    response = response.json()
+
+    insert_query = """INSERT INTO gists ("github_id", "html_url", "git_pull_url", "git_push_url", "commits_url", "forks_url", "public", "created_at", "updated_at", "comments", "comments_url") 
+    VALUES (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, :created_at, :updated_at, :comments, :comments_url);"""
+
+    for gist in response:
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url'],
+        }
+        db.execute(insert_query, params)
+        if commit:
+            db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,19 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    #Check if there are keyword args
+    if not kwargs:
+        query = "SELECT * FROM gists"
+    #Check if github_id in kwargs
+    elif 'github_id' in kwargs:
+        query = "SELECT * FROM gists WHERE github_id = :github_id"
+    #Check if created_at in kwargs
+    elif 'created_at' in kwargs:
+        query = "SELECT * FROM gists WHERE datetime(created_at) = datetime(:created_at)"
+    
+    cursor = db_connection.execute(query,kwargs)
+    result = []
+    for gist in cursor:
+        result.append(Gist(gist))
+    
+    return result
